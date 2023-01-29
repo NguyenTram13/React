d5b5fbb8dcfff5cfb68f783b9d8a9fd0bdfb6139
Nguyen Tram <ngoctrambu13012001@gmail.com>
2023-01-29 19:34:03 +0700

    router

---
+++ src/components/BlogPage.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+const BlogPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("search"));
+  useEffect(() => {
+    setSearchParams({ search: "evondev" });
+  }, []);
+  return <div> Blog Page</div>;
+};
+
+export default BlogPage;
